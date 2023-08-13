0f981e85d5b911b04715d77016d06e2288a56939
Ryan Zhu <rz.ryanzhu@gmail.com>
2023-08-13 14:36:01 -0400

    logs, 404

---
+++ pages/404.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Head from "next/head";
+import Nav from "../components/nav";
+import Button from "../components/button";
+
+export default function _404() {
+  return (
+    <div className="w-screen h-screen bg-gradient-to-br from-slate-950 via-slate-900 to-slate-950">
+      <Head>
+        <title>MuseLab</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Nav />
+      <div className="flex flex-col items-center justify-center w-full h-full bg-[url('/assets/background.png')] bg-no-repeat bg-cover px-10 md:px-20 lg:px-32">
+        <h1 className="text-6xl md:text-7xl lg:text-8xl xl:text-9xl text-white font-bold">
+          404
+        </h1>
+        <h2 className="text-2xl text-white font-bold mb-10">Page not found</h2>
+        <Button onClick="/" text="Back to home" />
+      </div>
+    </div>
+  );
+}
---
+++ pages/admin/logs.js
@@ -0,0 +1,164 @@
+import React, { useState, useEffect } from "react";
+import Head from "next/head";
+import Spinner from "../../components/spinner";
+import Nav from "../../components/nav";
+
+import { showError, showSuccess } from "../../utils/verify";
+import Button from "../../components/button";
+
+export default function Admin() {
+  const [authenticated, setAuthenticated] = useState(false);
+  const [authToken, setAuthToken] = useState(null);
+
+  const [content, setContent] = useState("");
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (!accessToken || !localStorage.getItem("isLoggedIn")) {
+      window.location.href = "/login";
+    }
+    setAuthToken(accessToken);
+    fetch("https://api.muselab.app/api/auth/roles", {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          response
+            .json()
+            .then((data) => {
+              if (!data.includes("ROLE_ADMIN")) {
+                window.location.href = "/";
+              } else setAuthenticated(true);
+            })
+            .catch((error) => {
+              showError(error.message);
+            });
+        } else throw new Error("Not authenticated");
+      })
+      .catch((error) => {
+        showError(error.message);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (authenticated) {
+      list();
+    }
+  }, [authenticated]);
+
+  const list = () => {
+    fetch("https://api.muselab.app/api/logging/list", {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${
+          authToken || localStorage.getItem("accessToken")
+        }`,
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          response
+            .json()
+            .then((data) => {
+              console.log(data);
+              setContent(JSON.stringify(data, null, 2));
+            })
+            .catch((error) => {
+              showError(error.message);
+            });
+        } else throw new Error("Not authenticated");
+      })
+      .catch((error) => {
+        showError(error.message);
+      });
+  };
+
+  const search = () => {
+    fetch("https://api.muselab.app/api/logging/search", {
+      method: "POST",
+      authorization: `Bearer ${
+        authToken || localStorage.getItem("accessToken")
+      }`,
+      body: JSON.stringify({
+        query: query,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          response
+            .json()
+            .then((data) => {
+              setContent(JSON.stringify(data, null, 2));
+            })
+            .catch((error) => {
+              showError(error.message);
+            });
+        } else throw new Error("Not authenticated");
+      })
+      .catch((error) => {
+        showError(error.message);
+      });
+  };
+
+  const id = () => {
+    fetch("https://api.muselab.app/api/logging/get/" + query, {
+      method: "GET",
+      authorization: `Bearer ${
+        authToken || localStorage.getItem("accessToken")
+      }`,
+    })
+      .then((response) => {
+        if (response.ok) {
+          response
+            .json()
+            .then((data) => {
+              setContent(JSON.stringify(data, null, 2));
+            })
+            .catch((error) => {
+              showError(error.message);
+            });
+        } else throw new Error("Not authenticated");
+      })
+      .catch((error) => {
+        showError(error.message);
+      });
+  };
+
+  return authenticated ? (
+    <div className="w-screen h-screen bg-gradient-to-br from-slate-950 via-slate-900 to-slate-950">
+      <Head>
+        <title>MuseLab</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Nav />
+      <main className="flex flex-col items-center justify-center w-full h-full bg-[url('/assets/background.png')] bg-no-repeat bg-cover px-10 md:px-20 lg:px-32">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-black text-slate-50 mb-10">
+          Logs
+        </h1>
+        <div className="w-full flex flex-row flex-wrap gap-3 items-center justify-center">
+          <input
+            type="text"
+            className="h-10 lg:h-12 px-3 rounded-lg bg-slate-600/80 text-white/70 focus:outline-none focus:ring-1 focus:ring-slate-600"
+            placeholder="Query/Id"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <Button onClick={list} text="List" />
+          <Button onClick={search} text="Search" />
+          <Button onClick={id} text="Id" />
+        </div>
+        <div className="w-full rounded-xl bg-slate-600/80 text-slate-50 text-base mt-10 p-7">
+          {content}
+        </div>
+      </main>
+    </div>
+  ) : (
+    <div>
+      <Spinner />
+    </div>
+  );
+}
